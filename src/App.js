f8b853564394e5c600a1ab0c5cba97e1c3e09a2f
Goutman-afk <76959822+Goutman-afk@users.noreply.github.com>
2022-04-15 11:08:18 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import NavBar from "./Components/NavBar/NavBar";
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
