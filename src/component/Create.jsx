f75807e1bcaba2e0bab700bd2485e6a282147981
Goutman-afk <huynhlong020700@gmail.com>
2022-04-17 23:56:36 +0700

    .

--- src/component/Create.jsx
+++ src/component/Create.jsx
@@ -1,46 +1,62 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import erc20 from '../ABI/ERC20';
-import marketAbi from '../ABI/Market';
+import erc20 from "../ABI/ERC20";
+import marketAbi from "../ABI/Market";
 import nft from "../nft.png";
-import { Button, FormGroup, FormControl, ControlLabel, Form } from "react-bootstrap";
+import {
+  Button,
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  Form,
+} from "react-bootstrap";
 
 const Create = () => {
+  const [data, setData] = useState({ address: "", tokenId: "", price: 0 });
+  // State = {
+  //   address: "",
+  //   tokenID: "",
+  //   price: 0,
+  // };
+  const create = async (e) => {
+    e.preventDefault();
 
-
-
-
-
-
-
-
+    console.log(data);
+  };
 
   return (
     <div>
       <div className="container">
         <div className="row">
-
           <div className="col-md-4 card h-100 text-center p-4 mt-4">
             <img src={nft} height="400px" width="400px" />
           </div>
           <div className="col-md-3  h-100  p-4 mt-4">
-            <Form>
+            <Form onSubmit={create}>
               <Form.Group className="md-1" controlId="formBasicEmail">
                 <Form.Label>NFT address</Form.Label>
-                <Form.Control  placeholder="Enter NFT address" />
-
+                <Form.Control
+                  onChange={(e) => setData({ address: e.target.value })}
+                  type="text"
+                  placeholder="Enter NFT address"
+                />
               </Form.Group>
-           
+
               <Form.Group className="md-1" controlId="formBasicEmail">
                 <Form.Label>Token ID</Form.Label>
-                <Form.Control  placeholder="Enter Token ID" />
-
+                <Form.Control
+                  onChange={(e) => setData({ tokenId: e.target.value })}
+                  placeholder="Enter Token ID"
+                />
               </Form.Group>
-
 
               <Form.Group className="md-1" controlId="formBasicPassword">
                 <Form.Label>Price</Form.Label>
-                <Form.Control placeholder="Price" />
+                <Form.Control
+                  onChange={(e) => setData({ price: e.target.value })}
+                  type="number"
+                  placeholder="Price"
+                />
               </Form.Group>
 
               <Button variant="primary" type="submit">
@@ -48,12 +64,10 @@ const Create = () => {
               </Button>
             </Form>
           </div>
-
-
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Create
+export default Create;
